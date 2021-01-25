a3883630d07b85212f8e3f6f21ee06a7120b5785
Grassblock1 <QQ2650628005@hotmail.com>
2021-01-25 14:13:04 +0800

    你没有体验过的船新版本！

---
+++ js/comment.js
@@ -0,0 +1,6 @@
+new Valine({
+            el: '#vcomments',
+            appId: 'c2SWScYw2HQyd73YvFsziQ1q-MdYXbMMI',
+            appKey: 'tkL9chyp3UTXYQgftBdiv64X',
+            placeholder: '留下您的足迹'
+        })
